38579c9ebb0a2990de4fd5c84d115bebbcdeb571
AlanMandriaza <alan.mandriaza@gmail.com>
2023-02-16 18:49:33 +0000

    js agregado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let randomNumber = Math.random() * 10;
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+  console.log("holi" + randomNumber);
+};
+let generateExcuse = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoindex = Math.floor(Math.random() * who.length);
+  let whatindex = Math.floor(Math.random() * what.length);
+  let whenindex = Math.floor(Math.random() * when.length);
+  return who[whoindex] + " " + what[whatindex] + " " + when[whenindex];
 };
